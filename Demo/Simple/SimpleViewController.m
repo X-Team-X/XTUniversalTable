377e1d2e303a84506c3ee0d910bc3cf3d8cd7d8a
吾问 <wuwen.xb@alibaba-inc.com>
2016-05-06 17:24:44 +0800

    Update demo

--- Demo/Simple/SimpleViewController.m
+++ Demo/Simple/SimpleViewController.m
@@ -8,6 +8,8 @@
 
 #import "SimpleViewController.h"
 #import "XTUniversalTable.h"
+#import <AFNetworking/AFNetworking.h>
+#import <SVProgressHUD/SVProgressHUD.h>
 
 @interface SimpleViewController () {
     NSArray<id<XTUTSection>> *_sectons;
@@ -27,43 +29,54 @@
 }
 
 - (void)loadData {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
-        NSString *JSONPath = [[NSBundle mainBundle] pathForResource:@"cities" ofType:@"json"];
-        NSData *JSONData = [NSData dataWithContentsOfFile:JSONPath];
-        NSDictionary<NSString *, NSArray *> *JSONObject = [NSJSONSerialization JSONObjectWithData:JSONData
-                                                                                          options:NSJSONReadingMutableContainers
-                                                                                            error:nil];
-        NSMutableArray *sections = [NSMutableArray array];
-        
-        NSArray<NSString *> *sortedKeys = [[JSONObject allKeys] sortedArrayUsingComparator:^NSComparisonResult(NSString *key1, NSString *key2) {
-            return [key1 compare:key2];
-        }];
-        
-        [sortedKeys enumerateObjectsUsingBlock:^(NSString * _Nonnull key, NSUInteger idx, BOOL * _Nonnull stop) {
-            // Rows
-            NSMutableArray *cityRows = [NSMutableArray array];
-            NSArray<NSDictionary *> *cities = JSONObject[key];
-            [cities enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull city, NSUInteger idx, BOOL * _Nonnull stop) {
-                XTUTRow *row = [XTUTRow rowWithIdentifier:@"cell"
-                                              classString:@"SimpleCell"
-                                                     data:city];
-                if (row) {
-                    [cityRows addObject:row];
-                }
-            }];
-            // Header
-            XTUTSectionHeaderFooter *header = [XTUTSectionHeaderFooter headerWithHeight:20.0f title:key];
-            // Section
-            XTUTSection *section = [XTUTSection sectionWithRows:cityRows
-                                                         header:header
-                                                         footer:nil];
-            [sections addObject:section];
-        }];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.tableProxy.sections = sections;
-            [self.tableView reloadData];
-        });
-    });
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD showWithStatus:@"loading"];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager setResponseSerializer:[AFHTTPResponseSerializer serializer]];
+    [manager GET:@"https://raw.githubusercontent.com/X-Team-X/XTUniversalTable/master/Demo/Resource/cities.json"
+      parameters:nil
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             [SVProgressHUD dismiss];
+             NSDictionary<NSString *, NSArray *> *JSONObject = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                                                               options:NSJSONReadingMutableContainers
+                                                                                                 error:nil];
+             
+             NSMutableArray *sections = [NSMutableArray array];
+             
+             NSArray<NSString *> *sortedKeys = [[JSONObject allKeys] sortedArrayUsingComparator:^NSComparisonResult(NSString *key1, NSString *key2) {
+                 return [key1 compare:key2];
+             }];
+             
+             [sortedKeys enumerateObjectsUsingBlock:^(NSString * _Nonnull key, NSUInteger idx, BOOL * _Nonnull stop) {
+                 // Rows
+                 NSMutableArray *cityRows = [NSMutableArray array];
+                 NSArray<NSDictionary *> *cities = JSONObject[key];
+                 [cities enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull city, NSUInteger idx, BOOL * _Nonnull stop) {
+                     XTUTRow *row = [XTUTRow rowWithIdentifier:@"cell"
+                                                   classString:@"SimpleCell"
+                                                          data:city];
+                     if (row) {
+                         [cityRows addObject:row];
+                     }
+                 }];
+                 // Header
+                 XTUTSectionHeaderFooter *header = [XTUTSectionHeaderFooter headerWithHeight:20.0f title:key];
+                 // Section
+                 XTUTSection *section = [XTUTSection sectionWithRows:cityRows
+                                                              header:header
+                                                              footer:nil];
+                 [sections addObject:section];
+             }];
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 self.tableProxy.sections = sections;
+                 [self.tableView reloadData];
+             });
+         }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             [SVProgressHUD dismiss];
+             NSLog(@"Failed with error: %@", error);
+         }];
 }
 
 @end
